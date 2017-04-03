5e215db593636e464244ba1164e37ba3e04ffb90
hiddenstockpile <nineteenhundred88@gmail.com>
2017-04-04 03:09:59 +0800

    [HSP] ordmt_
    added config-sample.php to have a sample config file requirement

---
+++ config-sample.php
@@ -0,0 +1,7 @@
+<?php
+	define('REQUEST_TOKEN_NAME', [ADD_REQUEST_TOKEN_NAME_HERE]);
+	define('REQUEST_TOKEN_PASSWORD', [ADD_REQUEST_TOKEN_PASSWORD_HERE]);
+	//you have to put the correct url here where you added this application. Example : 'http://'. $_SERVER['SERVER_NAME'] . '/ordmt_'
+	define('HOME_URL', [ADD_HOME_URL_HERE]);
+	define('API_URL', [ADD_API_URL_HERE]); // PATH_TO_SERVER/wp-json for WP-API
+	define('API_NAMESPACE', [ADD_API_NAMESPACE_HERE]);//  "wp/v2" in this application for WP-API 
