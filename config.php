5ba014dfd4c8765f7a2bb52713da2aabc7d12776
hiddenstockpile <nineteenhundred88@gmail.com>
2017-04-04 02:59:06 +0800

    [HSP] ordmt_
    Should not be included in the first place

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('REQUEST_TOKEN_NAME', 'test');
-	define('REQUEST_TOKEN_PASSWORD', '4WB@mgar$#DVq8&%sBt(rj!5');
-	define('HOME_URL', 'http://'. $_SERVER['SERVER_NAME'] . '/ordmt_01');
-	define('API_URL', 'http://devel2.ordermate.online/wp-json');
-	define('API_NAMESPACE', 'wp/v2');
